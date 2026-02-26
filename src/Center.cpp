06c37f7e32f74ee623b8fffbacfe1cd4a84b47e7
akurfurst <219035144+akurfurst@users.noreply.github.com>
2026-02-26 19:27:23 +0000

    Refactored center class into its own file

---
+++ src/Center.cpp
@@ -0,0 +1,19 @@
+#include "Center.h"
+
+bn::fixed_point Center::position() {
+    return _sprite.position();
+}
+
+bn::fixed Center::mass() {
+    return _mass;
+}
+
+void Center::update() {
+    dPadMoveSprite(_sprite, _speed);
+}
+
+Center::Center(bn::fixed_point starting_position, bn::fixed mass, bn::fixed speed) : 
+    _sprite(bn::sprite_items::square.create_sprite(starting_position)),
+    _mass(mass),
+    _speed(speed) {
+}
